e32becd4f02f21c26fddefa5b76345d38f202c11
rebecaaras <rebecaaraujo2013@gmail.com>
2025-01-07 17:04:21 -0300

    new: Torna o componente Product reutilizável e ajusta estilos para que ProductsList seja rolável (scrollable)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,9 @@
-export default function Product(){
+export default function Product(props){
+    const {name, price, description, isAvailable} = props
     return(
         <div className="flex  flex-row justify-between mt-1 w-full py-2 px-2 bg-[#ced8e3] rounded-md sm:text-sm">
-            <h3>Nome do Produto</h3>
-            <span>R$ 50,00</span>
+            <h3>{name}</h3>
+            <span>R$ {price}</span>
         </div>
     )
 }
