fa46b5016b22b3a9c18b868f979f9b84a04545ca
rebecaaras <rebecaaraujo2013@gmail.com>
2025-01-08 10:54:29 -0300

    new: Adiciona ícones da lib lucide-react e insere essa dependência

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,21 @@
-export default function Product(props){
-    const {name, price, description, isAvailable} = props
-    return(
-        <div className="flex  flex-row justify-between mt-1 w-full py-2 px-2 bg-[#ced8e3] rounded-md sm:text-sm">
-            <h3>{name}</h3>
-            <span>R$ {price}</span>
-        </div>
-    )
+import { Trash2, Pencil, ChevronRight } from 'lucide-react';
+
+export default function Product(props) {
+  const { name, price, description, isAvailable } = props;
+  return (
+    <div className="flex  flex-row justify-between mt-1 w-full py-2 px-2 bg-[#ced8e3] rounded-md sm:text-sm">
+      <span className='flex flex-row items-center'>
+        <ChevronRight size={22}/>
+        <h3>{name}</h3>
+      </span>
+      <span className='flex flex-row items-center justify-between flex-start w-[130px]'>
+        <span className='mr-3 font-semibold'>R$ {price}</span>
+        <span className='flex flex-row w-[40px] justify-between'>
+            <Pencil size={18}/>
+            <Trash2 size={18}/>
+        </span>
+
+      </span>
+    </div>
+  );
 }
