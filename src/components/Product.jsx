aec780db30d6e4f6916ad8ed3cf4c39681c9e7d0
rebecaaras <rebecaaraujo2013@gmail.com>
2025-01-11 17:07:56 -0300

    new: Torna card de produto colapsável e ajusta estilos. Remove ícone de editar produto

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,34 @@
-import { Trash2, Pencil, ChevronRight } from 'lucide-react';
+import { Trash2, ChevronRight } from 'lucide-react';
+import { useState } from 'react';
 
 export default function Product(props) {
+  const [collapsed, setCollapsed] = useState(false);
   const { name, price, description, isAvailable, productId} = props;
 
   return (
-    <li key={productId} className="flex  flex-row justify-between mt-1 w-full py-2 px-2 bg-[#ced8e3] rounded-md sm:text-sm">
-      <span className='flex flex-row items-center'>
-        <ChevronRight size={22}/>
-        <h3>{name}</h3>
-      </span>
-      <span className='flex flex-row items-center justify-between flex-start w-[130px]'>
-        <span className='mr-3 font-semibold'>R$ {price}</span>
-        <span className='flex flex-row w-[40px] justify-between'>
-            <Pencil size={18}/>
-            <Trash2 size={18}/>
+    <li id={productId} className="w-full bg-[#ced8e3] mt-1 py-2 px-2 rounded-md sm:text-sm">
+      <div className='flex  flex-row justify-between font-semibold'>
+        <span className='flex flex-row items-center justify-center hover:cursor-pointer' onClick={() => setCollapsed(!collapsed)}>
+          { collapsed ? 
+            <ChevronRight size={22}/> :  <ChevronRight size={22} className="rotate-90 text-slate-500"/>
+          }
+          <h3>{name}</h3>
         </span>
-      </span>
+        <div className='flex flex-row items-center justify-between flex-start w-[130px]'>
+          <span className='mr-3 font-semibold'>R$ {price}</span>
+          <button 
+            className='flex justify-center items-center hover:bg-white rounded-sm p-[3px] hover:p-[2px] hover:border-[2px] hover:border-slate-900'
+            onClick>
+            <Trash2 size={18}/>
+          </button>
+        </div>
+      </div>
+      {!collapsed && 
+        <div className='h-auto items-center mt-1 pl-4 h-auto text-[0.8rem]'>
+          <p>{`Disponibilidade: ${isAvailable}`}</p>
+          <p>{`Descrição: ${description}`}</p>
+      </div>
+      }
     </li>
   );
 }
