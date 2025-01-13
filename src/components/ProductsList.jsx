7825a72afd7a3208f9678f5ff41f49504c028b28
rebecaaras <rebecaaraujo2013@gmail.com>
2025-01-13 10:26:55 -0300

    styles: Edita barra de rolagem e ajusta tamanho de ProductList para caber no seu elemento pai

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,6 +1,6 @@
 export default function ProductsList({children}) {
   return (
-    <div className="space-y-5 w-[80vw] h-96">
+    <div className="space-y-5 w-full">
       <h3 className="font-bold w-full text-center">Lista de Produtos</h3>
       <ul className="flex overflow-y-auto flex-wrap max-h-[80%]" id="product-list">
         {children}
